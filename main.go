0f405920642618005f32dbd63515e064c4d5c86b
Arjan Schaaf <arjan.schaaf@luminis.eu>
2017-10-02 09:56:08 +0200

    let's try weak etags

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 func handler(w http.ResponseWriter, r *http.Request) {
 
     // adding debug header to test (strong/weak) ETags in combination with NGINX
-    w.Header().Set("ETag", "HelloWorld")
-    w.Header().Set("Custom-ETag", "HelloWorld")
+    w.Header().Set("ETag", "W/HelloWorld")
+    w.Header().Set("Custom-ETag", "W/HelloWorld")
 
     fmt.Fprintln(w, "<b>Look for the ETag & Custom-ETag response headers! :-)</b>")
 
